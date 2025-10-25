2e4acb811698c8992064272c2175a11e5490cfc1
TiagoSansao <tiagossansao@gmail.com>
2025-10-25 10:27:46 -0300

    feat(binary_expression_tree): tree is being built successfully

---
+++ binary_expression_tree.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Node
+{
+  Node *left;
+  Node *right;
+  string value;
+};
+
+int findLowestPredecenceOperatorIndex(const string &infixExpression)
+{
+  for (int i = infixExpression.size() - 1; i >= 0; i--)
+  {
+    if (infixExpression[i] == '+' || infixExpression[i] == '-')
+    {
+      return i;
+    }
+  }
+
+  for (int i = infixExpression.size() - 1; i >= 0; i--)
+  {
+    if (infixExpression[i] == '/' || infixExpression[i] == '*')
+    {
+      return i;
+    }
+  }
+
+  return NULL;
+}
+
+Node *buildTree(Node *root)
+{
+  int lowestPredecenceOperatorIndex = findLowestPredecenceOperatorIndex(root->value);
+  if (lowestPredecenceOperatorIndex == NULL)
+    return nullptr;
+
+  Node *leftNode = new Node;
+  leftNode->value = root->value.substr(0, lowestPredecenceOperatorIndex);
+  root->left = leftNode;
+  buildTree(leftNode);
+
+  Node *rightNode = new Node;
+  rightNode->value = root->value.substr(lowestPredecenceOperatorIndex + 1);
+  root->right = rightNode;
+  buildTree(rightNode);
+
+  root->value = root->value[lowestPredecenceOperatorIndex];
+
+  return root;
+}
+
+int main()
+{
+  string infixExpression;
+
+  cin >> infixExpression;
+
+  Node *root = new Node();
+  root->value = infixExpression;
+  buildTree(root);
+
+  return 0;
+}
